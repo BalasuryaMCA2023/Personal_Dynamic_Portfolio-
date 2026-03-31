bb0ca15598cc451a5dfdcf3107943b59995445f5
BALASURYA S <139736173+BalasuryaMCA2023@users.noreply.github.com>
2026-04-01 01:05:22 +0530

    Change API proxy URL in vite.config.js
    Updated API proxy URL to point to the dynamic portfolio backend.

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,36 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 
   server: {
     proxy: {
-      '/api': 'http://localhost:8080' 
-    },
-    historyApiFallback: true
+      '/api': 'https://dynamic-portfolio-backend-gbv9.onrender.com'
+    }
   },
 
   build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
-              return 'vendor-react';
-            }
-            if (id.includes('framer-motion')) {
-              return 'vendor-motion';
-            }
-            if (id.includes('@mui') || id.includes('lucide-react') || id.includes('react-icons') || id.includes('bootstrap')) {
-              return 'vendor-ui';
-            }
-            return 'vendor'; // all other node_modules
-          }
-        }
-      }
-    }
+    chunkSizeWarningLimit: 1000
   }
 })
